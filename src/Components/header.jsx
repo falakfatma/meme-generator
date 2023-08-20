bacdcf78be232dce789d82b314b3a87279b7edf6
falakfatma <102820083+falakfatma@users.noreply.github.com>
2023-08-20 09:36:39 +0000

    add ramdom image

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,7 +1,8 @@
 export default function header() {
   return (
     <div className="flex justify-between bg-purple-800 p-5 text-white">
-      <h1 className="text-2xl">
+      <h1 className="text-2xl flex">
+      {/* <img src='https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Trollface_non-free.png/220px-Trollface_non-free.png' className="w-10 mx-4"/> */}
       Meme Generator
       </h1>
       <h3 className="text-xl">
--- src/Components/main.jsx
+++ src/Components/main.jsx
@@ -1,13 +1,27 @@
+import MemeData from './data'
+let url
 export default function Main() {
+  const clickHandlerFun = () => {
+    console.log(MemeData)
+    let memesArray = MemeData.data.memes
+    let randomImage = Math.floor(Math.random() * memesArray.length)
+    // for (let i=0; i<=lengthOfMeme; i++){
+    console.log(randomImage)
+    url = memesArray[randomImage].url
+    // }
+  }
+
   return (
     <div className='py-16 w-10/12 m-auto'>
       <div className="py-5 flex justify-center ">
         <input className='border-2 border-slate-300 p-2 px-4 rounded mx-4' />
         <input className='border-2 border-slate-300 p-2 px-4 rounded mx-4' />
       </div>
-      <button className='text-center bg-purple-800 w-full p-3 text-xl font-bold rounded text-white'>
+      <button onClick={clickHandlerFun} className='text-center bg-purple-800 w-full p-3 text-xl font-bold rounded text-white'>
         Get a new meme image
       </button>
+      <p>{url}</p>
+
     </div>
   )
 }
